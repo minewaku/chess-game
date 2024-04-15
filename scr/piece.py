478c4072ed9333c89d4831543268a3f6c31a2c7a
minewaku <97866342+minewaku@users.noreply.github.com>
2024-04-15 21:34:54 +0700

    fix 2 move forward of pawn can eat allies

--- scr/piece.py
+++ scr/piece.py
@@ -54,7 +54,7 @@ class Pawn(Piece):
             if self.isValidCoordinate(board.BOARD_SIZE, originX - 1, originY + 1) and board.board[originX - 1][originY + 1].point.piece != None and board.board[originX - 1][originY + 1].point.piece.side != self.side:
                 self.moveSet.append((originX - 1, originY + 1))
             
-            if originX == 6:
+            if originX == 6 and board.board[originX - 2][originY].point.piece != None:
                 self.moveSet.append((originX - 2, originY))
 
 
@@ -68,7 +68,7 @@ class Pawn(Piece):
             if self.isValidCoordinate(board.BOARD_SIZE, originX + 1, originY + 1) and board.board[originX + 1][originY + 1].point.piece != None and board.board[originX + 1][originY + 1].point.piece.side != self.side:
                 self.moveSet.append((originX + 1, originY + 1))
             
-            if originX == 1 :
+            if originX == 1 and board.board[originX + 2][originY].point.piece != None:
                 self.moveSet.append((originX + 2, originY))
 
         return self.moveSet
